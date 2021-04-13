976351a74f836b7814689ed37d5728fbee9027a3
Yash Kant <ysh.kant@gmail.com>
2021-04-13 17:03:39 -0400

    Make config Sokoban compatible

--- habitat_baselines/utils/env_utils.py
+++ habitat_baselines/utils/env_utils.py
@@ -91,7 +91,7 @@ def construct_envs(
             task_config.DATASET.CONTENT_SCENES = scene_splits[i]
 
         task_config.SIMULATOR.HABITAT_SIM_V0.GPU_DEVICE_ID = (
-            config.SIMULATOR_GPU_ID
+            config.SIMULATOR_GPU_ID[i % len(config.SIMULATOR_GPU_ID)]
         )
 
         task_config.SIMULATOR.AGENT_0.SENSORS = config.SENSORS
